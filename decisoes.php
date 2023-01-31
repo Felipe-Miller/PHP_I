734c374d29811e0a9fa9f549a5bfd9bac8f3382c
Felipe-Miller <fmvargas@protonmail.com>
2023-01-31 16:44:50 -0300

    .

---
+++ decisoes.php
@@ -0,0 +1,12 @@
+<?php
+
+$idade = 17;
+
+echo "Você só pode entrar se tiver mais do que 18 anos..." . PHP_EOL;
+
+if ($idade  >= 18) {
+    echo "Você tem $idade anos." . PHP_EOL;
+    echo 'Pode entrar.';
+}
+
+echo "Você só tem $idade anos, não poderá entrar. ";
