04e63fda016b5ef2465234919373e2089b4df637
Felipe-Miller <fmvargas@protonmail.com>
2023-01-31 11:08:26 -0300

    .

--- strings.php
+++ strings.php
@@ -6,4 +6,4 @@ echo "Olá mundo!" . PHP_EOL;
 echo "Eu tenho $idade anos.";
 echo PHP_EOL . PHP_EOL;
 echo "Você só pode entrar, se tiver mais de 18 anos...\n";
-echo "Você tem $idade anos. Pode entrar.";
+echo "Você tem $idade anos. Pode entrar. ";
