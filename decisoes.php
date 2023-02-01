c55384cfcbe5f4f78cf4e303baa1872a8a1b87c9
Felipe-Miller <email@youremail.com>
2023-01-31 22:39:25 -0300

    .

--- decisoes.php
+++ decisoes.php
@@ -1,12 +1,32 @@
 <?php
 
 $idade = 17;
+$numeroDePessoas = 2;
 
-echo "Você só pode entrar se tiver mais do que 18 anos..." . PHP_EOL;
+    echo "Você só pode entrar se tiver mais do que 18 anos ou 16 anos acompanhado de um responsável." . PHP_EOL;
 
-if ($idade  >= 18) {
-    echo "Você tem $idade anos." . PHP_EOL;
-    echo 'Pode entrar.';
-}
+if ($idade  >= 18) 
 
-echo "Você só tem $idade anos, não poderá entrar. ";
+    echo "Você tem $idade anos, pode entrar. ";
+
+else if ($idade >= 16 && $numeroDePessoas >= 2) 
+
+    echo "Você tem $idade anos, está acompanhado, então pode entrar. ";
+
+else 
+
+    echo "Você só tem $idade anos, não poderá entrar. " . PHP_EOL;
+    echo "Adeus! ";
+
+
+
+     
+  
+
+    
+
+   
+
+
+
+
